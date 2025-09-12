fa45876d5a8d01d4a57417b00c059d226dacf131
izzy <dadhoz@gmail.com>
2025-09-12 12:19:39 +0200

    agregar matrices de tiempo de centros educativos y optimizacion de script para matrizes de tiempo

--- Analysis/traveltimeMatrix.py
+++ Analysis/traveltimeMatrix.py
@@ -1,63 +1,96 @@
 import pandas as pd
 import geopandas as gpd
 import os
+import sys
+from datetime import datetime, time
 from os import listdir
 from os.path import join
 import r5py
-import datetime
+from r5py import TransportMode
 
-listGTFSpaths = [
-    '../gtfsData/busesEMT_9may2025.zip', # buses EMT
-    '../gtfsData/metro_30may2025.zip', # metro
-    '../gtfsData/metroLigero_26feb2025.zip', # metro ligero
-    '../gtfsData/cercanias_23july2025_fromRenfe.zip' # cercanias
-] + [join('../gtfsData/busesInterurbanos_Urbanos_all_16july2025/', i) for i in listdir('../gtfsData/busesInterurbanos_Urbanos_all_16july2025/')]
 
-madridOSMpath = '../osmData/madrid-latest.osm.pbf' 
+def definingOrigin():
+    municipioOrigen = 0
 
-transport_network = r5py.TransportNetwork(
-    madridOSMpath,
-    listGTFSpaths,
-    allow_errors=True
-)
+    munisDone = [i[6:-4] for i in listdir(f'./timeMatrices/{destType}/')]
 
-destType = 'templos'
-with open('../muni2024/madridMetroArea/muniList.txt') as f:
-    municipios = [i[:-6] for i in f.readlines()]
+    # print(municipios)
+    # print(munisDone)
 
-munisDone = [i[6:-4] for i in listdir(f'./timeMatrices/{destType}/')]
+    for muni in municipios:
+        if muni in munisDone:
+            continue
+        else:
+            municipioOrigen = muni
+            break
 
-# print(municipios)
-# print(munisDone)
+    return municipioOrigen
+   
+def main(municipioOrigen):
 
-for muni in municipios:
-    if muni in munisDone:
-        continue
-    else:
-        municipioOrigen = muni
-        break
+    if municipioOrigen == 0:
+        prtStr = 'all origins processed ' + '-' * 10 + datetime.now().strftime("%I:%M%p")
+        print(prtStr.rjust(30, '-'))
+        return -1
+
+    pointOrigins =gpd.read_file(f'../muni2024/madridMetroArea/{municipioOrigen}.gpkg').rename({'GRD_ID' : 'id'}, axis=1)
+    pointOrigins = pointOrigins.to_crs('EPSG:3035')
+
+    pointDestinations = gpd.read_file('../POIs/educacion/cleaned/centrosEducativos_secundaria.geojson').rename({'centro_codigo' : 'id'}, axis=1)
+    pointDestinations = pointDestinations.to_crs('EPSG:3035')
+
+    print(f'{datetime.today().strftime("%I:%M%p")} : {pointOrigins.shape[0]} origins in {municipioOrigen} ---- {pointDestinations.shape[0]} destinations')
+
+    travel_time_matrix = r5py.TravelTimeMatrix(
+        transport_network= transport_network,
+        origins = pointOrigins,
+        destinations = pointDestinations,
+        transport_modes = [TransportMode.TRANSIT, TransportMode.WALK, TransportMode.TRAM, TransportMode.SUBWAY, TransportMode.RAIL, TransportMode.BUS],
+        access_modes = [TransportMode.WALK],
+        departure = datetime(2025, 7, 28, 14, 0, 0)
+    )
+
+    travel_time_matrix = pd.DataFrame(travel_time_matrix)
+
+    travel_time_matrix.to_csv(f'./timeMatrices/{destType}/desde_{municipioOrigen}.csv')
+
+    return 0
+
+
+if __name__ == '__main__':
+
+    print('building transport network...', end = '')
+
+    listGTFSpaths = [
+        '../gtfsData/busesEMT_9may2025.zip', # buses EMT
+        '../gtfsData/metro_30may2025.zip', # metro
+        '../gtfsData/metroLigero_26feb2025.zip', # metro ligero
+        '../gtfsData/cercanias_23july2025_fromRenfe.zip' # cercanias
+    ] + [join('../gtfsData/busesInterurbanos_Urbanos_all_16july2025/', i) for i in listdir('../gtfsData/busesInterurbanos_Urbanos_all_16july2025/')]
+
+    madridOSMpath = '../osmData/madrid-latest.osm.pbf' 
+
+    transport_network = r5py.TransportNetwork(
+        madridOSMpath,
+        listGTFSpaths,
+        allow_errors=True
+    )
+
+    sys.stdout.flush()
+    print('\rfinding next origins...'.ljust(50, ' '))
+
+    destType = 'secundaria'
+    with open('../muni2024/madridMetroArea/muniList.txt') as f:
+        municipios = [i[:-6] for i in f.readlines()]
     
+    done = False
+    while not done:
+        muni = definingOrigin()
+        x = main(muni)
+        if x == -1:
+            done = True
 
-pointOrigins =gpd.read_file(f'../muni2024/madridMetroArea/{municipioOrigen}.gpkg').rename({'GRD_ID' : 'id'}, axis=1)
-pointOrigins = pointOrigins.to_crs('EPSG:3035')
-
-pointDestinations = gpd.read_file('../POIs/templos/cleaned/templosMadrid.geojson').rename({'PK' : 'id'}, axis=1)
-pointDestinations = pointDestinations.to_crs('EPSG:3035')
-
-print(f'{pointOrigins.shape[0]} origins in {municipioOrigen} ---- {pointDestinations.shape[0]} destinations')
-
-travel_time_matrix = r5py.TravelTimeMatrix(
-    transport_network= transport_network,
-    origins = pointOrigins,
-    destinations = pointDestinations,
-    transport_modes = [r5py.TransportMode.TRANSIT],
-    departure=datetime.datetime(2025, 7, 28, 14, 0, 0)
-)
-
-travel_time_matrix = pd.DataFrame(travel_time_matrix)
-
-travel_time_matrix.to_csv(f'./timeMatrices/{destType}/desde_{municipioOrigen}.csv')
-
-duration = 0.25  # seconds
-freq = 400  # Hz
-os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq))
+    duration = 0.25  # seconds
+    freq = 400  # Hz
+    os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq))
+    
