46bc4d27fa0d50376dcf1697088f30889636d753
izzy <dadhoz@gmail.com>
2025-09-19 19:41:15 +0200

    actualizar matrices para bibliotecas

--- Analysis/traveltimeMatrix.py
+++ Analysis/traveltimeMatrix.py
@@ -8,6 +8,7 @@ from os.path import join
 import r5py
 from r5py import TransportMode
 
+DEST = 'bibliotecas'
 
 def definingOrigin():
     municipioOrigen = 0
@@ -38,8 +39,17 @@ def main(municipioOrigen):
 
     pointOrigins['snapped_geometry'] = transport_network.snap_to_network(pointOrigins["geometry"])
 
-    pointDestinations = gpd.read_file('../POIs/educacion/cleaned/centrosEducativos_secundaria.geojson').rename({'centro_codigo' : 'id'}, axis=1)
-    pointDestinations = pointDestinations.to_crs('EPSG:3035')
+
+    if DEST == 'secundaria':
+        pointDestinations = gpd.read_file('../POIs/educacion/cleaned/centrosEducativos_secundaria.geojson').rename({'centro_codigo' : 'id'}, axis=1)
+        pointDestinations = pointDestinations.to_crs('EPSG:3035')
+    elif DEST == 'bibliotecas':
+        pointDestinations = gpd.read_file('../POIs/bibliotecas/cleaned/bibliotecasTodo.geojson').rename({'PK' : 'id'}, axis=1)
+        pointDestinations = pointDestinations.to_crs('EPSG:3035')
+    elif DEST == 'universidades':
+        pointDestinations = gpd.read_file('../POIs/educacion/cleaned/centrosEducativos_universidades.geojson').rename({'centro_codigo' : 'id'}, axis=1)
+        pointDestinations = pointDestinations.to_crs('EPSG:3035')
+
 
     print(f'{datetime.today().strftime("%I:%M%p")} : {pointOrigins.shape[0]} origins in {municipioOrigen} ---- {pointDestinations.shape[0]} destinations')
 
@@ -81,7 +91,7 @@ if __name__ == '__main__':
     sys.stdout.flush()
     print('\rfinding next origins...'.ljust(50, ' '))
 
-    destType = 'secundaria'
+    destType = DEST
     with open('../muni2024/madridMetroArea/muniList.txt') as f:
         municipios = [i[:-6] for i in f.readlines()]
     
